9e8387450db64ac3d3640e5db21e4481f7651acf
anythinglord <sjsalcedoa@unal.edu.co>
2024-05-27 10:18:06 -0500

    define a simple palette, change layout look

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+import { NavBar } from './components/NavBar/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='container'>
-      <div className="item">a</div>
-      <div className="item">b</div>
-      <div className="item">c</div>
-      <div className="item">d</div>
+    <div>
+      <NavBar />
+      <div className='container'>
+        <div className="item">a</div>
+        <div className="item">b</div>
+        <div className="item">c</div>
+        <div className="item">d</div>
+      </div>
     </div>
   )
 }
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+export const NavBar: React.FC = () => {
+  return (
+    <div className='nb-root'>
+      <div className="nb-title">
+        Building App  
+      </div>
+    </div>
+  )
+}
